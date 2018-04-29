587fc0dc9f46964e3af923216018921703910f21
Özgür Yalçın <yalcin.ozqur@gmail.com>
2018-04-29 21:16:58 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ func main() {
 	// Müşteri kaydı oluşturmak için
 	if auth {
 		request := parasut.Request{}
-		request.Contacts.Data.Type = "contacts"
-		request.Contacts.Data.Attr.AccountType = "customer" // customer (Müşteri) || supplier (Tedarikçi)
+		request.Contacts.Data.Type = "contacts"             // << Burada değişiklik yapmayınız !
+		request.Contacts.Data.Attr.AccountType = "customer" // << Burada değişiklik yapmayınız !
 		request.Contacts.Data.Attr.Name = ""                // Firma Ünvanı
 		request.Contacts.Data.Attr.ShortName = ""           // Kısa İsim
 		request.Contacts.Data.Attr.ContactType = ""         // company (Şirket) || person (Şahıs)
@@ -35,11 +35,11 @@ func main() {
 	// Çalışan kaydı oluşturmak için
 	if auth {
 		request := parasut.Request{}
-		request.Employees.Data.Type = "employees"
-		request.Employees.Data.Attr.Name = ""  // İsim
-		request.Employees.Data.Attr.Email = "" // E-posta adresi
-		request.Employees.Data.Attr.TCKN = ""  // TC Kimlik Numarası
-		request.Employees.Data.Attr.IBAN = ""  // IBAN numarası
+		request.Employees.Data.Type = "employees" // << Burada değişiklik yapmayınız !
+		request.Employees.Data.Attr.Name = ""     // İsim
+		request.Employees.Data.Attr.Email = ""    // E-posta adresi
+		request.Employees.Data.Attr.TCKN = ""     // TC Kimlik Numarası
+		request.Employees.Data.Attr.IBAN = ""     // IBAN numarası
 		response := api.CreateEmployee(request)
 		pretty, _ := json.MarshalIndent(response.Employees, " ", "\t")
 		fmt.Println(string(pretty))
@@ -61,14 +61,14 @@ func main() {
 		response := api.ShowSalesInvoice(request)
 		docid := response.SalesInvoices.Data.RelationShips.ActiveEDocument.Data.ID
 		doctype := response.SalesInvoices.Data.RelationShips.ActiveEDocument.Data.Type
-		if doctype == "e_archives" {
+		if doctype == "e_archives" { // Fatura tipi e-Arşiv ise
 			request := parasut.Request{}
 			request.EArchives.Data.ID = docid
 			earchive := api.ShowEArchive(request)
 			pretty, _ := json.MarshalIndent(earchive.EArchives, " ", "\t")
 			fmt.Println(string(pretty))
 		}
-		if doctype == "e_invoices" {
+		if doctype == "e_invoices" { // Fatura tipi e-Fatura ise
 			request := parasut.Request{}
 			request.EInvoices.Data.ID = docid
 			response := api.ShowEInvoice(request)
@@ -84,14 +84,14 @@ func main() {
 		response := api.ShowSalesInvoice(request)
 		docid := response.SalesInvoices.Data.RelationShips.ActiveEDocument.Data.ID
 		doctype := response.SalesInvoices.Data.RelationShips.ActiveEDocument.Data.Type
-		if doctype == "e_archives" {
+		if doctype == "e_archives" { // Fatura tipi e-Arşiv ise
 			request := parasut.Request{}
 			request.EArchivePDF.Data.ID = docid
 			response := api.ShowEArchivePDF(request)
 			pdfurl := response.EArchivePDF.Data.Attr.URL
 			fmt.Println(pdfurl)
 		}
-		if doctype == "e_invoices" {
+		if doctype == "e_invoices" { // Fatura tipi e-Fatura ise
 			request := parasut.Request{}
 			request.EInvoicePDF.Data.ID = docid
 			response := api.ShowEInvoicePDF(request)
